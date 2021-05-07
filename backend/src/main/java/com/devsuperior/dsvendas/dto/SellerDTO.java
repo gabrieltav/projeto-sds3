82ea8c336cfe7a0cc288f260538d6d4953a1ffe7
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-06 21:48:06 -0400

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDTO implements Serializable { /*Uma "boa medida" para garantir que os objetos
+													SellerDTO possam ser convertidos para bytes
+													garante que o objeto possa ser trafegado em rede,
+													salvo em arquivos*/
+	
+	private static final long serialVersionUID = 1L; //versão 1
+	private Long id;
+	private String name;
+	
+	public SellerDTO() {
+	}
+
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
