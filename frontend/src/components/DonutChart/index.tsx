dead0cb3dc64c9641eca591b58886513597e74c2
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-05-08 15:18:21 -0400

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,6 +24,7 @@ const DonutChart = () => {
             });
     }, []);
 
+    //test
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
